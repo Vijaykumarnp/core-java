ed15ac1c8e8da22112afa6f31369ba5662132e75
Vijaykumarnp <vijaykumar.xworkz@gmail.com>
2022-12-16 17:47:03 +0530

    uploading object orinted files

---
+++ App - Copy.java
@@ -0,0 +1,12 @@
+class App{
+
+
+public static void main(String a[]){
+String name = "bababababba";
+char c[]= name.tocharArray();
+for(int i =c.length-1 ; i >= 0 ; i--){
+	System.out.println(c[i]);
+
+}	
+}
+}
--- Child.java
+++
@@ -1,7 +0,0 @@
-class Child extends Parent{
-
-
-
-
-
-}
